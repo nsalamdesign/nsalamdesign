a052fb46e3fd322c2f6742fb8e3b0923298b0f1a
nsalamdesign <nsalamdesign@gmail.com>
2026-06-18 07:06:19 +0100

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
@@ -69,6 +70,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S509X0HKSK"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-S509X0HKSK');
+          `}
+        </Script>
+
         <Navbar />
 
         <PageWrapper>
