dd4a5474530a5fc8f44a6365a733ddb4cb5f63c6
nsalamdesign <nsalamdesign@gmail.com>
2026-06-18 15:23:36 +0100

    Fix sitemap domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,23 +3,23 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://nsalamdesign.com",
+      url: "https://nsalamdesign.vercel.app",
       lastModified: new Date(),
     },
     {
-      url: "https://nsalamdesign.com/portfolio",
+      url: "https://nsalamdesign.vercel.app/portfolio",
       lastModified: new Date(),
     },
     {
-      url: "https://nsalamdesign.com/blog",
+      url: "https://nsalamdesign.vercel.app/blog",
       lastModified: new Date(),
     },
     {
-      url: "https://nsalamdesign.com/pricing",
+      url: "https://nsalamdesign.vercel.app/pricing",
       lastModified: new Date(),
     },
     {
-      url: "https://nsalamdesign.com/contact",
+      url: "https://nsalamdesign.vercel.app/contact",
       lastModified: new Date(),
     },
   ];
